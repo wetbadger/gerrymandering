88ec1dff2c4db4f43d76610be561743b85dfd50b
Badger <wetbadger49@gmail.com>
2022-07-19 23:36:53 -0400

    menu works sort of. need assets and fix scrolling

--- UI/GotoMenu.gd
+++
@@ -1,7 +0,0 @@
-extends Button
-
-
-
-
-func _on_GotoMenu_button_up():
-	get_tree().change_scene("res://Menus/MainMenu/MainMenu.tscn")
--- UI/Winner.gd
+++
@@ -1,8 +0,0 @@
-extends Label
-
-func _ready():
-	pass
-
-
-func set_border_color(color):
-	get_font("font").outline_color = color
